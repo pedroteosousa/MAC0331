0d28507123a45cc8c98ade5a768de54e32df427c
pedroteosousa <pedroteosousa@gmail.com>
2018-10-23 11:49:35 -0300

    Add setUp at utils tests

--- code/test_utils.py
+++ code/test_utils.py
@@ -4,9 +4,10 @@ from math import inf
 import random
 import utils
 
-random.seed(0)
-
 class TestUtils (unittest.TestCase):
+    def setUp(self):
+        random.seed(278)
+
     def test_count_intersections(self):
         l = [Line(7, 3), Line(-5, 9), Line(-1, 5), Line(-9, 60)]
         self.assertEqual(utils.intersections(l, (-inf, inf))[0], 6)
@@ -17,7 +18,7 @@ class TestUtils (unittest.TestCase):
     def test_random_inversion(self):
         l = [Line(7, 3), Line(-5, 9), Line(-1, 5), Line(-9, 60)]
         _, inv = utils.intersections(l, (-inf, inf))
-        self.assertEqual(inv, (l[2], l[1]))
+        self.assertEqual(inv, (l[0], l[1]))
         _, inv = utils.intersections(l, (2, 3))
         self.assertEqual(inv, None)
         _, inv = utils.intersections(l, (3, 4))
