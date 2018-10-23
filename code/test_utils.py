f9fa90297cc52484ccf9d08867e7c75f35bc98f2
Pedro Sousa <pedroteosousa@gmail.com>
2018-10-23 21:15:18 -0200

    Use same set of lines on all utils tests

--- code/test_utils.py
+++ code/test_utils.py
@@ -6,30 +6,33 @@ import utils
 
 class TestUtils (unittest.TestCase):
     def setUp(self):
-        random.seed(278)
+        self.G1 = [Line(2, 7), Line(-1, -2), Line(-5, 0), Line(-0.2, 1), Line(-0.5, 3), Line(-5, 10), Line(0, 4)]
+        self.G2 = [Line(4, -6), Line(-3, -5), Line(0, 5), Line(8, 9), Line(1, 6)]
+        random.seed(0)
 
     def test_count_intersections(self):
-        l = [Line(7, 3), Line(-5, 9), Line(-1, 5), Line(-9, 60)]
-        self.assertEqual(utils.intersections(l, (-inf, inf))[0], 6)
-        self.assertEqual(utils.intersections(l, (0, 10))[0], 5)
-        self.assertEqual(utils.intersections(l, (0, 2))[0], 3)
-        self.assertEqual(utils.intersections(l, (3, inf))[0], 3)
+        G = self.G1
+        self.assertEqual(utils.intersections(G, (-inf, inf))[0], 20)
+        self.assertEqual(utils.intersections(G, (0, 10))[0], 7)
+        self.assertEqual(utils.intersections(G, (-1, 2))[0], 9)
+        self.assertEqual(utils.intersections(G, (-3, inf))[0], 16)
 
     def test_random_inversion(self):
-        l = [Line(7, 3), Line(-5, 9), Line(-1, 5), Line(-9, 60)]
-        _, inv = utils.intersections(l, (-inf, inf))
-        self.assertEqual(inv, (l[0], l[1]))
-        _, inv = utils.intersections(l, (2, 3))
+        G = self.G1
+        inv = utils.intersections(G, (-inf, inf))[1]
+        self.assertEqual(inv, (G[6], G[2]))
+        inv = utils.intersections(G, (2, 3))[1]
         self.assertEqual(inv, None)
-        _, inv = utils.intersections(l, (3, 4))
-        self.assertEqual(inv, (l[0], l[3]))
+        inv = utils.intersections(G, (3, 4))[1]
+        self.assertEqual(inv, (G[1], G[5]))
 
     def test_level(self):
         G = [Line(2, 7), Line(-1, -2), Line(-5, 0), Line(-0.2, 1), Line(-0.5, 3)]
+        G = self.G1
         self.assertEqual(utils.level(G, 1, -10), G[3])
-        self.assertEqual(utils.level(G, 2, -10), G[4])
-        self.assertEqual(utils.level(G, 3, -2), G[4])
-        self.assertEqual(utils.level(G, 0, 0), G[1])
+        self.assertEqual(utils.level(G, 2, -10), G[6])
+        self.assertEqual(utils.level(G, 3, -2), G[6])
+        self.assertEqual(utils.level(G, 6, 0), G[5])
         self.assertEqual(utils.level(G, 0, inf), G[2])
         self.assertEqual(utils.level(G, 0, -inf), G[0])
 
