cc1a12061ea3ad7b818069da4f22870369de93dd
pedroteosousa <pedroteosousa@gmail.com>
2018-10-23 11:03:45 -0300

    Change tabs to space

--- code/basic.py
+++ code/basic.py
@@ -3,100 +3,101 @@ from math import inf
 epsilon = 1e-10
 
 def nearly_equal(a, b):
-	return abs(a-b) <= epsilon
+    return abs(a-b) <= epsilon
 
 class Point:
-	"Um ponto de coordenadas (x,y)"
+    "Um ponto de coordenadas (x,y)"
 
-	def __init__ (self, x, y):
-		"Cria um ponto a partir de suas coordenadas"
-		self.x, self.y = x, y
+    def __init__ (self, x, y):
+        "Cria um ponto a partir de suas coordenadas"
+        self.x, self.y = x, y
 
-	def __eq__ (self, p):
-		"Verifica se dois pontos são iguais"
-		return nearly_equal(self.x, p.x) and nearly_equal(self.y, p.y)
+    def __eq__ (self, p):
+        "Verifica se dois pontos são iguais"
+        return nearly_equal(self.x, p.x) and nearly_equal(self.y, p.y)
 
-	def above (self, l):
-		"Verifica se o ponto está acima da reta"
-		return self.y > l.m * self.x + l.b 
+    def above (self, l):
+        "Verifica se o ponto está acima da reta"
+        return self.y > l.m * self.x + l.b 
 
-	def above_in (self, l):
-		"Verifica se o ponto está acima ou na reta"
-		return self.above(l) or self in l
+    def above_in (self, l):
+        "Verifica se o ponto está acima ou na reta"
+        return self.above(l) or self in l
 
-	def under (self, l):
-		"Verifica se o ponto está abaixo da reta"
-		return not self.above_in(l);
+    def under (self, l):
+        "Verifica se o ponto está abaixo da reta"
+        return not self.above_in(l);
 
-	def under_in (self, l):
-		"Verifica se o ponto está abaixo ou na reta"
-		return not self.above(l);
+    def under_in (self, l):
+        "Verifica se o ponto está abaixo ou na reta"
+        return not self.above(l);
 
-	def dual (self):
-		"Reta referente ao dual do ponto em questão"
-		return Line(self.x, -self.y)
+    def dual (self):
+        "Reta referente ao dual do ponto em questão"
+        return Line(self.x, -self.y)
 
 class Line:
-	"Uma reta não vertical que obedece y=mx+b"
-	
-	@classmethod
-	def cmp(self, x):
-		"Retorna uma função que compara as retas no x dado"
-		def cmp_x(l1, l2):
-			t1, t2 = (0, 0), (0, 0)
-			if x == inf:
-				t1, t2 = (l1.m, l1.b), (l2.m, l2.b)
-			elif x == -inf:
-				t1, t2 = (-l1.m, -l1.b), (-l2.m, -l2.b)
-			else:
-				t1, t2 = (l1(x), -l1.m), (l2(x), -l2.m)
-			return -1 if t1 < t2 else 0 if t1 == t2 else 1
-		return cmp_x
+    "Uma reta não vertical que obedece y=mx+b"
 
-	def __init__ (self, m, b):
-		"Cria uma reta a partir de seus parâmetros"
-		self.m, self.b = m, b
+    @classmethod
+    def cmp(self, x):
+        "Retorna uma função que compara as retas no x dado"
+        def cmp_x(l1, l2):
+            t1, t2 = (0, 0), (0, 0)
+            if x == inf:
+                t1, t2 = (l1.m, l1.b), (l2.m, l2.b)
+            elif x == -inf:
+                t1, t2 = (-l1.m, -l1.b), (-l2.m, -l2.b)
+            else:
+                t1, t2 = (l1(x), -l1.m), (l2(x), -l2.m)
+            return -1 if t1 < t2 else 0 if t1 == t2 else 1
+        return cmp_x
 
-	def __eq__ (self, l):
-		"Verifica se duas retas são iguais"
-		return nearly_equal(self.m, l.m) and nearly_equal(self.b, l.b)
+    def __init__ (self, m, b):
+        "Cria uma reta a partir de seus parâmetros"
+        self.m, self.b = m, b
 
-	def __contains__ (self, p):
-		"Verifica se o ponto está contido na reta"
-		return nearly_equal(p.y, self.m * p.x + self.b)
-	
-	def __call__ (self, x):
-		"y da reta no ponto x"
-		return self.m * x + self.b
+    def __eq__ (self, l):
+        "Verifica se duas retas são iguais"
+        return nearly_equal(self.m, l.m) and nearly_equal(self.b, l.b)
 
-	def __hash__ (self):
-		"Hash da reta"
-		return hash((self.m, self.b))
+    def __contains__ (self, p):
+        "Verifica se o ponto está contido na reta"
+        return nearly_equal(p.y, self.m * p.x + self.b)
 
-	def horizontal (self):
-		"Verifica se a reta é horizontal"
-		return nearly_equal(self.m, 0)
+    def __call__ (self, x):
+        "y da reta no ponto x"
+        return self.m * x + self.b
 
-	def parallel (self, l):
-		"Verifica se duas retas são paralelas"
-		return nearly_equal(self.m, l.m)
+    def __hash__ (self):
+        "Hash da reta"
+        return hash((self.m, self.b))
 
-	def perpendicular (self, l):
-		"Verifica se duas retas são perpendiculares"
-		if self.horizontal() or l.horizontal():
-			return false
-		return nearly_equal(-1.0, l.m * self.m)
+    def horizontal (self):
+        "Verifica se a reta é horizontal"
+        return nearly_equal(self.m, 0)
 
-	def intersect (self, l):
-		"Intersecção de duas retas (pode ser nada, um ponto ou uma reta)"
-		if self == l:
-			return Line(self.m, self.b)
-		if self.parallel(l):
-			return None
-		x = -(self.b - l.b) / (self.m - l.m)
-		y = self.m * x + self.b
-		return Point(x, y)
+    def parallel (self, l):
+        "Verifica se duas retas são paralelas"
+        return nearly_equal(self.m, l.m)
 
-	def dual (self):
-		"Ponto referente ao dual da reta em questão"
-		return Point(self.m, -self.b)
+    def perpendicular (self, l):
+        "Verifica se duas retas são perpendiculares"
+        if self.horizontal() or l.horizontal():
+            return false
+        return nearly_equal(-1.0, l.m * self.m)
+
+    def intersect (self, l):
+        "Intersecção de duas retas (pode ser nada, um ponto ou uma reta)"
+        if self == l:
+            return Line(self.m, self.b)
+        if self.parallel(l):
+            return None
+        x = -(self.b - l.b) / (self.m - l.m)
+        y = self.m * x + self.b
+        return Point(x, y)
+
+    def dual (self):
+        "Ponto referente ao dual da reta em questão"
+        return Point(self.m, -self.b)
+
--- code/test_basic.py
+++ code/test_basic.py
@@ -3,136 +3,136 @@ from basic import Point, Line
 from math import inf
 
 class TestPoint (unittest.TestCase):
-	def test_init (self):
-		p = Point(-20.5, 25)
-		self.assertEqual((p.x, p.y), (-20.5, 25))
-		q = Point(0, 0)
-		self.assertEqual((q.x, q.y), (0, 0))
-		r = Point(135,-135)
-		self.assertEqual((r.x, r.y), (135, -135))
+    def test_init (self):
+        p = Point(-20.5, 25)
+        self.assertEqual((p.x, p.y), (-20.5, 25))
+        q = Point(0, 0)
+        self.assertEqual((q.x, q.y), (0, 0))
+        r = Point(135,-135)
+        self.assertEqual((r.x, r.y), (135, -135))
 
-	def test_eq (self):
-		a = Point(1, 10)
-		b = Point(1, 10)
-		c = Point(0, 0)
-		self.assertEqual(a, b)
-		self.assertNotEqual(a, c)
-		self.assertNotEqual(b, c)
+    def test_eq (self):
+        a = Point(1, 10)
+        b = Point(1, 10)
+        c = Point(0, 0)
+        self.assertEqual(a, b)
+        self.assertNotEqual(a, c)
+        self.assertNotEqual(b, c)
 
-	def test_above (self):
-		a = Point(10, 10)
-		b = Point(12, 13)
+    def test_above (self):
+        a = Point(10, 10)
+        b = Point(12, 13)
 
-	def test_dual (self):
-		a = Point(0, 0)
-		self.assertEqual(a.dual(), Line(0, 0))
-		b = Point(0, -1)
-		self.assertEqual(b.dual(), Line(0, 1))
-		c = Point(-1, 2)
-		self.assertEqual(c.dual(), Line(-1, -2))
-		d = Point(-0.5,-2)
-		self.assertEqual(d.dual(), Line(-0.5, 2))
+    def test_dual (self):
+        a = Point(0, 0)
+        self.assertEqual(a.dual(), Line(0, 0))
+        b = Point(0, -1)
+        self.assertEqual(b.dual(), Line(0, 1))
+        c = Point(-1, 2)
+        self.assertEqual(c.dual(), Line(-1, -2))
+        d = Point(-0.5,-2)
+        self.assertEqual(d.dual(), Line(-0.5, 2))
 
 class TestLine (unittest.TestCase):
-	def test_cmp (self):
-		l = Line(3, 5)
-		g = Line(-4, 2)
-		# x = -inf
-		self.assertLess(Line.cmp(-inf)(l, g), 0)
-		self.assertGreater(Line.cmp(-inf)(g, l), 0)
-		# x = -3/7
-		self.assertEqual(Line.cmp(-3/7)(l, g), -1)
-		self.assertEqual(Line.cmp(-3/7)(g, l), 1)
-		# x = 7
-		self.assertGreater(Line.cmp(7)(l, g), 0)
-		self.assertLess(Line.cmp(7)(g, l), 0)
-		# x = +inf
-		self.assertGreater(Line.cmp(inf)(l, g), 0)
-		self.assertLess(Line.cmp(inf)(g, l), 0)
+    def test_cmp (self):
+        l = Line(3, 5)
+        g = Line(-4, 2)
+        # x = -inf
+        self.assertLess(Line.cmp(-inf)(l, g), 0)
+        self.assertGreater(Line.cmp(-inf)(g, l), 0)
+        # x = -3/7
+        self.assertEqual(Line.cmp(-3/7)(l, g), -1)
+        self.assertEqual(Line.cmp(-3/7)(g, l), 1)
+        # x = 7
+        self.assertGreater(Line.cmp(7)(l, g), 0)
+        self.assertLess(Line.cmp(7)(g, l), 0)
+        # x = +inf
+        self.assertGreater(Line.cmp(inf)(l, g), 0)
+        self.assertLess(Line.cmp(inf)(g, l), 0)
 
-	def test_init (self):
-		val = (5.5, -5)
-		l = Line(*val)
-		self.assertEqual((l.m, l.b), val)
-	
-	def test_eq (self):
-		l = Line (-5, 9)
-		g = Line (-5, 9)
-		h = Line (-5, -9)
-		self.assertEqual(l, g)
-		self.assertNotEqual(l, h)
-	
-	def test_contains (self):
-		a = Point(1, 1)
-		b = Point(-12, -12)
-		c = Point(27, -12)
-		x = Line(1, 0)
-		y = Line(-1, 2)
-		z = Line(0, -12)
-		self.assertTrue (a in x)
-		self.assertTrue (a in y)
-		self.assertFalse(a in z)
-		self.assertTrue (b in x)
-		self.assertFalse(b in y)
-		self.assertTrue (b in z)
-		self.assertFalse(c in x)
-		self.assertFalse(c in y)
-		self.assertTrue (c in z)
+    def test_init (self):
+        val = (5.5, -5)
+        l = Line(*val)
+        self.assertEqual((l.m, l.b), val)
 
-	def test_call (self):
-		l = Line(5, 1/3)
-		self.assertAlmostEqual(l(2), 31/3)
-		self.assertAlmostEqual(l(0), 1/3)
-	
-	def test_hash (self):
-		l = Line(-9, 1/3)
-		self.assertEqual(hash(l), hash((l.m, l.b)))
-	
-	def test_horizontal (self):
-		l = Line(0, -8)
-		g = Line(0.00001, -5)
-		self.assertTrue(l.horizontal())
-		self.assertFalse(g.horizontal())
-	
-	def test_parallel (self):
-		l = Line(5, 10)
-		g = Line(5, -125)
-		self.assertTrue(l.parallel(g))
-		self.assertTrue(g.parallel(l))
+    def test_eq (self):
+        l = Line (-5, 9)
+        g = Line (-5, 9)
+        h = Line (-5, -9)
+        self.assertEqual(l, g)
+        self.assertNotEqual(l, h)
 
-		# horizontal lines
-		l = Line (0, 0)
-		g = Line (0, -5)
-		self.assertTrue(l.parallel(g))
-	
-	def test_perpendicular (self):
-		l = Line(.5, 10)
-		g = Line(-2, 550)
-		h = Line(1, 5)
-		self.assertTrue(l.perpendicular(g))
-		self.assertTrue(g.perpendicular(l))
-		self.assertFalse(l.perpendicular(h))
+    def test_contains (self):
+        a = Point(1, 1)
+        b = Point(-12, -12)
+        c = Point(27, -12)
+        x = Line(1, 0)
+        y = Line(-1, 2)
+        z = Line(0, -12)
+        self.assertTrue (a in x)
+        self.assertTrue (a in y)
+        self.assertFalse(a in z)
+        self.assertTrue (b in x)
+        self.assertFalse(b in y)
+        self.assertTrue (b in z)
+        self.assertFalse(c in x)
+        self.assertFalse(c in y)
+        self.assertTrue (c in z)
 
-	def test_intersect (self):
-		# None
-		l = Line(5, 10)
-		g = Line(5, -125)
-		self.assertEqual(l.intersect(g), None)	
+    def test_call (self):
+        l = Line(5, 1/3)
+        self.assertAlmostEqual(l(2), 31/3)
+        self.assertAlmostEqual(l(0), 1/3)
 
-		# Point
-		l = Line(6, 8)
-		g = Line(-1, -3)
-		self.assertEqual(l.intersect(g), Point(-11.0/7.0, -10.0/7.0))
+    def test_hash (self):
+        l = Line(-9, 1/3)
+        self.assertEqual(hash(l), hash((l.m, l.b)))
 
-		# Line
-		l = Line(10, -1/3)
-		g = Line(10, -1/3)
-		self.assertEqual(l.intersect(g), l)
+    def test_horizontal (self):
+        l = Line(0, -8)
+        g = Line(0.00001, -5)
+        self.assertTrue(l.horizontal())
+        self.assertFalse(g.horizontal())
 
-	def test_dual (self):
-		l = Line(3.3, 8)
-		p = Point(3.3, -8)
-		self.assertEqual(l.dual(), p)
+    def test_parallel (self):
+        l = Line(5, 10)
+        g = Line(5, -125)
+        self.assertTrue(l.parallel(g))
+        self.assertTrue(g.parallel(l))
+
+        # horizontal lines
+        l = Line (0, 0)
+        g = Line (0, -5)
+        self.assertTrue(l.parallel(g))
+
+    def test_perpendicular (self):
+        l = Line(.5, 10)
+        g = Line(-2, 550)
+        h = Line(1, 5)
+        self.assertTrue(l.perpendicular(g))
+        self.assertTrue(g.perpendicular(l))
+        self.assertFalse(l.perpendicular(h))
+
+    def test_intersect (self):
+        # None
+        l = Line(5, 10)
+        g = Line(5, -125)
+        self.assertEqual(l.intersect(g), None)	
+
+        # Point
+        l = Line(6, 8)
+        g = Line(-1, -3)
+        self.assertEqual(l.intersect(g), Point(-11.0/7.0, -10.0/7.0))
+
+        # Line
+        l = Line(10, -1/3)
+        g = Line(10, -1/3)
+        self.assertEqual(l.intersect(g), l)
+
+    def test_dual (self):
+        l = Line(3.3, 8)
+        p = Point(3.3, -8)
+        self.assertEqual(l.dual(), p)
 
 if __name__ == "__main__":
-	unittest.main()
+    unittest.main()
